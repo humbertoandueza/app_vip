e1fae3ad9c69cef33f12c121e669ed476948dc71
Humberto <humbertoanduezaa@gmail.com>
2018-06-05 11:41:34 -0430

    panel_django

--- application/app/urls.py
+++ application/app/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from .views import *
 urlpatterns = [
     url(r'^$', index,name="index"),
+    url(r'^panel/$', panel,name="panel"),
+
 ]
--- application/app/views.py
+++ application/app/views.py
@@ -6,3 +6,5 @@ from django.shortcuts import redirect
 # Create your views here.
 def index(request):
 	return render(request,'landing_page/index.html')
+def panel(request):
+	return render(request,'panel_admin/index.html')
